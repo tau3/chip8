912589f664112d1e6fb3044e5c29fa70b696fc70
tau3 <smr.oznob@gmail.com>
2020-04-15 20:23:10 +0400

    buffer struct - initial implementation

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,5 @@
 use std::fs::File;
-use std::intrinsics::transmute;
 use std::io::Read;
-use std::panic::resume_unwind;
 use std::thread;
 use std::time::Duration;
 
@@ -58,16 +56,59 @@ static FOURTH_FOUR_BITS_MASK_16: u16 = 0x000F;
 static THIRD_FOUR_BITS_MASK_16: u16 = 0x00F0;
 static SECOND_FOUR_BITS_MASK_16: u16 = 0x0F00;
 
+struct Buffer<T: Clone + From<u8>> {
+    buffer: Vec<T>,
+}
+
+impl<T: Clone + From<u8>> Buffer<T> {
+    fn new(size: usize) -> Buffer<T> {
+        let memory = vec![(0 as u8).into(); size];
+        Buffer { buffer: memory }
+    }
+}
+
+impl<T: Clone + From<u8>> std::ops::Index<usize> for Buffer<T> {
+    type Output = T;
+
+    fn index(&self, index: usize) -> &Self::Output {
+        &self.buffer[index]
+    }
+}
+
+impl<T: Clone + From<u8>> std::ops::Index<u16> for Buffer<T> {
+    type Output = T;
+
+    fn index(&self, index: u16) -> &Self::Output {
+        &self.buffer[index as usize]
+    }
+}
+
+impl<T: Clone + From<u8>> std::ops::Index<u8> for Buffer<T> {
+    type Output = T;
+
+    fn index(&self, index: u8) -> &Self::Output {
+        &self.buffer[index as usize]
+    }
+}
+
+impl<T: Clone + From<u8>> std::ops::Index<i32> for Buffer<T> {
+    type Output = T;
+
+    fn index(&self, index: i32) -> &Self::Output {
+        &self.buffer[index as usize]
+    }
+}
+
 struct Chip8 {
     opcode: u16,
-    memory: [u8; 4096],
-    v_registers: [u8; 16],
+    memory: Buffer<u8>,
+    v_registers: Buffer<u8>,
     index_register: u16,
     program_counter: u16,
-    gfx: [u8; 64 * 32],
+    gfx: Buffer<u8>,
     delay_timer: u8,
     sound_timer: u8,
-    stack: [u16; 16],
+    stack: Buffer<u16>,
     sp: u16,
     key: [u8; 16],
     draw_flag: bool,
@@ -77,14 +118,14 @@ impl Chip8 {
     fn new() -> Self {
         // reset everything
         let opcode: u16 = 5;
-        let mut memory: [u8; 4096] = [0; 4096];
-        let v_registers: [u8; 16] = [0; 16];
+        let mut memory = Buffer::new(4096);
+        let v_registers = Buffer::new(16);
         let index_register: u16 = 0;
         let program_counter: u16 = 0x200;
-        let gfx: [u8; 64 * 32] = [0; 64 * 32];
+        let gfx = Buffer::new(64 * 32);
         let delay_timer: u8 = 0;
         let sound_timer: u8 = 0;
-        let stack: [u16; 16] = [0; 16];
+        let stack = Buffer::new(16);
         let sp: u16 = 0;
         let key: [u8; 16] = [0; 16];
         let draw_flag = false;
@@ -93,8 +134,6 @@ impl Chip8 {
         for i in 0..80 {
             memory[i] = CHIP8_FONTSET[i];
         }
-        // load program
-        read_binary(&mut memory[512..]);
 
         Chip8 {
             opcode,
@@ -119,14 +158,14 @@ impl Chip8 {
 
     fn emulate_cycle(&mut self) {
         // fetch opcode (two bytes)
-        self.opcode = self.memory[self.program_counter << 8] | self.memory[self.program_counter + 1];
+        self.opcode = ((self.memory[self.program_counter] as u16) << 8) | self.memory[self.program_counter + 1].into();
 
         // decode opcode
         match self.opcode & FIRST_FOUR_BITS_MASK_16 {
             // ANNN: Sets index register to the address NNN
             0xA000 => {
                 self.index_register = self.opcode & LAST_TWELVE_BITS_MASK_16;
-                self.pc += 2;
+                self.program_counter += 2;
             }
             0x0000 => {
                 match self.opcode & FOURTH_FOUR_BITS_MASK_16 {
@@ -136,18 +175,18 @@ impl Chip8 {
                 }
             }
             0x2000 => {
-                self.stack[self.sp] = self.pc;
-                sp += 1;
-                self.pc = self.opcode & LAST_TWELVE_BITS_MASK_16;
+                self.stack[self.sp] = self.program_counter;
+                self.sp += 1;
+                self.program_counter = self.opcode & LAST_TWELVE_BITS_MASK_16;
             }
             0x0004 => {
                 if self.v_registers[(self.opcode & THIRD_FOUR_BITS_MASK_16) >> 4] > 0xFF - self.v_registers[(self.opcode & 0xF00) >> 8] {
-                    self.v_registers[0xF] = 1; // carry
+                    self.v_registers[0xF] = 1u8; // carry
                 } else {
-                    self.v_registers[0xF] = 0;
+                    self.v_registers[0xF] = 0u8;
                 }
                 self.v_registers[(self.opcode & THIRD_FOUR_BITS_MASK_16 >> 8)] += self.v_registers[(self.opcode & THIRD_FOUR_BITS_MASK_16) >> 4];
-                pc += 2;
+                self.program_counter += 2;
             }
             0x0033 => {
                 self.memory[self.index_register] = self.v_registers[(self.opcode & SECOND_FOUR_BITS_MASK_16) >> 8] / 100;
@@ -158,15 +197,15 @@ impl Chip8 {
                 let x: u8 = self.v_registers[(self.opcode & SECOND_FOUR_BITS_MASK_16) >> 8];
                 let y: u8 = self.v_registers[(self.opcode & SECOND_FOUR_BITS_MASK_16) >> 4];
                 let height: u8 = (self.opcode & FOURTH_FOUR_BITS_MASK_16) as u8;
-                self.v_registers[0xF] = 0;
+                self.v_registers[0xF] = 0u8;
                 for yline in 0..height {
                     let pixel = self.memory[self.index_register + yline as u16];
                     for xline in 0..8 {
                         if (pixel & (0x80 >> xline)) != 0 {
-                            if gfx[(x + xline + ((y + yline) * 64))] == 1 {
-                                self.v_registers[0xf] = 1;
+                            if self.gfx[(x + xline + ((y + yline) * 64))] == 1 {
+                                self.v_registers[0xf] = 1u8;
                             }
-                            gfx[(x + xline + ((y + yline) * 64))] ^= 1;
+                            self.gfx[(x + xline + ((y + yline) * 64))] ^= 1;
                         }
                     }
                 }

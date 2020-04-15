8c4d6e99718c2573840759e4db3aa3d6b7a7b4ff
tau3 <smr.oznob@gmail.com>
2020-04-15 19:11:49 +0400

    use mask constants

--- src/main.rs
+++ src/main.rs
@@ -2,21 +2,11 @@ use std::fs::File;
 use std::intrinsics::transmute;
 use std::io::Read;
 use std::panic::resume_unwind;
+use std::thread;
+use std::time::Duration;
 
 fn main() {
-    let opcode: u16 = 5;
-    let memory: [u8; 4096] = [0; 4096];
-    let v_registers: [u8; 16] = [0; 16];
-    let index_register: u16 = 0;
-    let program_counter: u16 = 0;
-    let gfx: [u8; 64 * 32] = [0; 64 * 32];
-    let delay_timer: u8 = 0;
-    let sound_timer: u8 = 0;
-
-    let stack: [u16; 16] = [0; 16];
-    let sp: u16;
-
-    let key: [u8; 16] = [0; 16];
+    game_loop();
 }
 
 fn setup_graphics() {}
@@ -30,11 +20,11 @@ fn game_loop() {
     setup_input();
 
     let mut chip8 = Chip8::new();
-    chip8.initialize();
-    chip8.load_game("pong");
+    chip8.load_game("pong2.c8");
 
     loop {
-        // TODO 60 cycles per second
+        thread::sleep(Duration::from_millis(1000 / 60));
+
         chip8.emulate_cycle();
         if chip8.is_draw_flag() {
             draw_graphics();
@@ -59,8 +49,14 @@ static CHIP8_FONTSET: [u8; 80] = [
     0xF0, 0x80, 0x80, 0x80, 0xF0, // C
     0xE0, 0x90, 0x90, 0x90, 0xE0, // D
     0xF0, 0x80, 0xF0, 0x80, 0xF0, // E
-    0xF0, 0x80, 0xF0, 0x80, 0x80,  // F
+    0xF0, 0x80, 0xF0, 0x80, 0x80, // F
 ];
+
+static FIRST_FOUR_BITS_MASK_16: u16 = 0xF000;
+static LAST_TWELVE_BITS_MASK_16: u16 = 0x0FFF;
+static FOURTH_FOUR_BITS_MASK_16: u16 = 0x000F;
+static THIRD_FOUR_BITS_MASK_16: u16 = 0x00F0;
+static SECOND_FOUR_BITS_MASK_16: u16 = 0x0F00;
 
 struct Chip8 {
     opcode: u16,
@@ -116,48 +112,52 @@ impl Chip8 {
         }
     }
 
-    fn initialize(&self) {}
-    fn load_game(&self, name: &str) {}
+    fn load_game(&mut self, name: &str) {
+        let mut file = File::open("pong").expect("failed to read game");
+        file.read_exact(&mut self.memory[512..]);
+    }
+
     fn emulate_cycle(&mut self) {
         // fetch opcode (two bytes)
         self.opcode = self.memory[self.program_counter << 8] | self.memory[self.program_counter + 1];
 
-        // decode opcode (first four bits)
-        match self.opcode & 0xF000 {
+        // decode opcode
+        match self.opcode & FIRST_FOUR_BITS_MASK_16 {
+            // ANNN: Sets index register to the address NNN
             0xA000 => {
-                self.index_register = self.opcode & 0x0FFF; // last 12 bits
+                self.index_register = self.opcode & LAST_TWELVE_BITS_MASK_16;
                 self.pc += 2;
             }
             0x0000 => {
-                match self.opcode & 0x000F { // second four bits
+                match self.opcode & FOURTH_FOUR_BITS_MASK_16 {
                     0x0000 => { /* 0x00E0: clear screen */ }
                     0x000E => { /* 0x000E: returns from subroutine */ }
-                    _ => { println!("[0x0000]: {} is not recognized", self.opcode) }
+                    _ => { println!("[0x0000]: {:X} is not recognized", self.opcode) }
                 }
             }
             0x2000 => {
                 self.stack[self.sp] = self.pc;
                 sp += 1;
-                self.pc = self.opcode & 0x0FFF;
+                self.pc = self.opcode & LAST_TWELVE_BITS_MASK_16;
             }
             0x0004 => {
-                if self.v_registers[(self.opcode & 0x00F0) >> 4] > 0xFF - self.v_registers[(self.opcode & 0xF00) >> 8] {
+                if self.v_registers[(self.opcode & THIRD_FOUR_BITS_MASK_16) >> 4] > 0xFF - self.v_registers[(self.opcode & 0xF00) >> 8] {
                     self.v_registers[0xF] = 1; // carry
                 } else {
                     self.v_registers[0xF] = 0;
                 }
-                self.v_registers[(self.opcode & 0x0F00 >> 8)] += self.v_registers[(self.opcode & 0x00F0) >> 4];
+                self.v_registers[(self.opcode & THIRD_FOUR_BITS_MASK_16 >> 8)] += self.v_registers[(self.opcode & THIRD_FOUR_BITS_MASK_16) >> 4];
                 pc += 2;
             }
             0x0033 => {
-                self.memory[self.index_register] = self.v_registers[(self.opcode & 0x0F00) >> 8] / 100;
-                self.memory[self.index_register + 1] = (self.v_registers[(self.opcode & 0x0F00) >> 8] / 10) % 10;
-                self.memory[self.index_register + 2] = (self.v_registers[(self.opcode & 0x0F00) >> 8] % 100) % 10;
+                self.memory[self.index_register] = self.v_registers[(self.opcode & SECOND_FOUR_BITS_MASK_16) >> 8] / 100;
+                self.memory[self.index_register + 1] = (self.v_registers[(self.opcode & SECOND_FOUR_BITS_MASK_16) >> 8] / 10) % 10;
+                self.memory[self.index_register + 2] = (self.v_registers[(self.opcode & SECOND_FOUR_BITS_MASK_16) >> 8] % 100) % 10;
             }
             0xD000 => {
-                let x: u8 = self.v_registers[(self.opcode & 0x0F00) >> 8];
-                let y: u8 = self.v_registers[(self.opcode & 0x0F00) >> 4];
-                let height: u8 = (self.opcode & 0x000F) as u8;
+                let x: u8 = self.v_registers[(self.opcode & SECOND_FOUR_BITS_MASK_16) >> 8];
+                let y: u8 = self.v_registers[(self.opcode & SECOND_FOUR_BITS_MASK_16) >> 4];
+                let height: u8 = (self.opcode & FOURTH_FOUR_BITS_MASK_16) as u8;
                 self.v_registers[0xF] = 0;
                 for yline in 0..height {
                     let pixel = self.memory[self.index_register + yline as u16];
@@ -173,7 +173,7 @@ impl Chip8 {
                 self.draw_flag = true;
                 self.program_counter += 2;
             }
-            _ => { println!("{} is not recognized", self.opcode) }
+            _ => { println!("{:X} is not recognized", self.opcode) }
         }
 
         // update timers
@@ -193,11 +193,6 @@ impl Chip8 {
     }
 
     fn set_keys(&self) {}
-}
-
-fn read_binary(buffer: &mut [u8]) {
-    let mut file = File::open("pong").expect("failed to read game");
-    file.read_exact(buffer);
 }
 
 fn beep() {}

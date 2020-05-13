3214af81d98edb70f80007b89cf9813318e38113
tau3 <smr.oznob@gmail.com>
2020-05-13 18:48:55 +0400

    add opcode trait

--- src/chip8.rs
+++ src/chip8.rs
@@ -1,6 +1,8 @@
 use std::fs::File;
 use std::io::Read;
 
+use rand::Rng;
+
 use crate::buffer::Buffer;
 
 pub struct Chip8 {
@@ -72,14 +74,40 @@ impl Chip8 {
 
         // decode opcode
         match self.opcode & FIRST_FOUR_BITS {
-            // ANNN: Sets index register to the address NNN
             0xA000 => {
                 self.ir = self.opcode & LAST_TWELVE_BITS;
                 self.pc += 2;
             }
+            0xC000 => {
+                let x = self.opcode.x();
+                let nn = (self.opcode & LAST_EIGHT_BITS) as u8;
+                let random_number: u8 = rand::thread_rng().gen_range(0, 255);
+                self.vr[x] = nn & random_number;
+                self.pc += 2;
+            }
+            0x8000 => {
+                match self.opcode & LAST_FOUR_BITS {
+                    0x0002 => {
+                        let x = self.opcode.x();
+                        let y = self.opcode.y();
+                        self.vr[x] &= self.vr[y];
+                        self.pc += 2;
+                    }
+                    0x0004 => {
+                        let x = self.opcode.x();
+                        if self.vr[(self.opcode & THIRD_FOUR_BITS) >> 4] > 0xFF - self.vr[x] {
+                            self.vr[0xFu8] = 1u8; // carry
+                        } else {
+                            self.vr[0xFu8] = 0u8;
+                        }
+                        self.vr[x] = self.vr[x].wrapping_add(self.vr[(self.opcode & THIRD_FOUR_BITS) >> 4]);
+                        self.pc += 2;
+                    }
+                    _ => { println!("[0x8000]: {:X} is not recognized", self.opcode) }
+                }
+            }
             0x0000 => {
-                match self.opcode & FOURTH_FOUR_BITS {
-//                    0x0000 => { /* 0x00E0: clear screen */ }
+                match self.opcode & LAST_FOUR_BITS {
                     0x000E => {
                         self.sp -= 1;
                         self.pc = self.stack[self.sp];
@@ -93,19 +121,10 @@ impl Chip8 {
                 self.sp += 1;
                 self.pc = self.opcode & LAST_TWELVE_BITS;
             }
-            0x0004 => {
-                if self.vr[(self.opcode & THIRD_FOUR_BITS) >> 4] > 0xFF - self.vr[(self.opcode & 0xF00) >> 8] {
-                    self.vr[0xFu8] = 1u8; // carry
-                } else {
-                    self.vr[0xFu8] = 0u8;
-                }
-                self.vr[(self.opcode & THIRD_FOUR_BITS >> 8)] += self.vr[(self.opcode & THIRD_FOUR_BITS) >> 4];
-                self.pc += 2;
-            }
             0xD000 => {
-                let x: u8 = self.vr[(self.opcode & SECOND_FOUR_BITS) >> 8];
+                let x: u8 = self.vr[self.opcode.x()];
                 let y: u8 = self.vr[(self.opcode & THIRD_FOUR_BITS) >> 4];
-                let height: u8 = (self.opcode & FOURTH_FOUR_BITS) as u8;
+                let height: u8 = (self.opcode & LAST_FOUR_BITS) as u8;
                 self.vr[0xFu8] = 0u8;
                 for yline in 0..height {
                     let pixel = self.memory[self.ir + yline as u16];
@@ -125,7 +144,16 @@ impl Chip8 {
             0xE000 => {
                 match self.opcode & LAST_EIGHT_BITS {
                     0x009E => {
-                        if self.key[self.vr[(self.opcode & SECOND_FOUR_BITS) >> 8]] != 0 {
+                        if self.key[self.vr[self.opcode.x()]] != 0 {
+                            self.pc += 4;
+                        } else {
+                            self.pc += 2;
+                        }
+                    }
+                    0x00A1 => {
+                        let x = self.opcode.x();
+                        let vx = self.vr[x];
+                        if self.key[vx] == 0 {
                             self.pc += 4;
                         } else {
                             self.pc += 2;
@@ -136,17 +164,17 @@ impl Chip8 {
             }
             0x6000 => {
                 // upper bits will be truncated while casting u16 to u8
-                self.vr[(self.opcode & SECOND_FOUR_BITS) >> 8] = (self.opcode & FIRST_EIGHT_BITS) as u8;
+                self.vr[self.opcode.x()] = (self.opcode & FIRST_EIGHT_BITS) as u8;
                 self.pc += 2;
             }
             0x7000 => {
-                let x = (self.opcode & SECOND_FOUR_BITS) >> 8;
+                let x = self.opcode.x();
                 let nn = (self.opcode & FIRST_EIGHT_BITS) as u8;
                 self.vr[x] += nn;
                 self.pc += 2;
             }
             0x3000 => {
-                let x = (self.opcode & SECOND_FOUR_BITS) >> 8;
+                let x = self.opcode.x();
                 let nn = (self.opcode & FIRST_EIGHT_BITS) as u8;
                 if self.vr[x] == nn {
                     self.pc += 4;
@@ -161,13 +189,13 @@ impl Chip8 {
             0xF000 => {
                 match self.opcode & LAST_EIGHT_BITS {
                     0x0033 => {
-                        self.memory[self.ir] = self.vr[(self.opcode & SECOND_FOUR_BITS) >> 8] / 100;
-                        self.memory[self.ir + 1] = (self.vr[(self.opcode & SECOND_FOUR_BITS) >> 8] / 10) % 10;
-                        self.memory[self.ir + 2] = (self.vr[(self.opcode & SECOND_FOUR_BITS) >> 8] % 100) % 10;
+                        self.memory[self.ir] = self.vr[self.opcode.x()] / 100;
+                        self.memory[self.ir + 1] = (self.vr[self.opcode.x()] / 10) % 10;
+                        self.memory[self.ir + 2] = (self.vr[self.opcode.x()] % 100) % 10;
                         self.pc += 2;
                     }
                     0x0065 => {
-                        let x = (self.opcode & SECOND_FOUR_BITS) >> 8;
+                        let x = self.opcode.x();
                         let mut offset = self.ir;
                         for i in 0..=x {
                             self.vr[i] = self.memory[offset];
@@ -176,17 +204,17 @@ impl Chip8 {
                         self.pc += 2;
                     }
                     0x0029 => {
-                        let x = (self.opcode & SECOND_FOUR_BITS) >> 8;
+                        let x = self.opcode.x();
                         self.ir = self.vr[x] as u16 * 5;
                         self.pc += 2;
                     }
                     0x0015 => {
-                        let x = (self.opcode & SECOND_FOUR_BITS) >> 8;
+                        let x = self.opcode.x();
                         self.delay_timer = x as u8;
                         self.pc += 2;
                     }
                     0x0007 => {
-                        let x = (self.opcode & SECOND_FOUR_BITS) >> 8;
+                        let x = self.opcode.x();
                         self.vr[x] = self.delay_timer;
                         self.pc += 2;
                     }
@@ -242,7 +270,7 @@ static CHIP8_FONTSET: [u8; 80] = [
 
 static FIRST_FOUR_BITS: u16 = 0xF000;
 static LAST_TWELVE_BITS: u16 = 0x0FFF;
-static FOURTH_FOUR_BITS: u16 = 0x000F;
+static LAST_FOUR_BITS: u16 = 0x000F;
 static THIRD_FOUR_BITS: u16 = 0x00F0;
 static SECOND_FOUR_BITS: u16 = 0x0F00;
 static LAST_EIGHT_BITS: u16 = 0x00FF;
@@ -250,3 +278,19 @@ static FIRST_EIGHT_BITS: u16 = 0xFF;
 
 pub static WIDTH: usize = 64;
 pub static HEIGHT: usize = 32;
+
+trait OpCode {
+    fn x(&self) -> u16;
+
+    fn y(&self) -> u16;
+}
+
+impl OpCode for u16 {
+    fn x(&self) -> u16 {
+        (self & SECOND_FOUR_BITS) >> 8
+    }
+
+    fn y(&self) -> u16 {
+        (self & THIRD_FOUR_BITS) >> 4
+    }
+}
